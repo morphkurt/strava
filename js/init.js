6bd928a9b8d5444fa0a55f0c777093efbd29564f
Gunawardena <Damitha.N.Gunawardena@team.telstra.com>
2019-02-22 10:45:30 +1100

    version 0.1

--- js/init.js
+++ js/init.js
@@ -4,11 +4,15 @@ var weeklyData;
 function loadData() {
 	$.getJSON( "https://web.damitha.xyz/stravaapi/activities/run/weekly", function( json ) {
   		weeklyData=json;
- 	});
+  		renderWeeklyLoadGraph(weeklyData);
+  		renderWeeklyPerformanceGraph(weeklyData);
+	});
 }
+
 
 $( document ).ready(function() {
 	loadData();
+	//renderWeeklyPerformanceGraph(weeklyData);
 });
 
 
